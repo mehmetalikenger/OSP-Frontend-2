7f9342f56b796519729e4179071a80566caf4ea9
Mehmet Ali Kenger <mehmetalikenger.dev@gmail.com>
2026-05-29 19:49:43 +0300

    ui polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,6 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans, Roboto_Condensed, Syncopate, Orbitron, Anta, Exo_2, Oxanium, Open_Sans } from "next/font/google";
+import { Oxanium, Open_Sans } from "next/font/google";
 import "./globals.css";
-
-const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["200", "300", "400", "500", "600", "700", "800"],
-  variable: "--font-plus-jakarta",
-});
-
-const robotoCondensed = Roboto_Condensed({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "700", "900"],
-  variable: "--font-roboto-condensed",
-});
-
-const syncopate = Syncopate({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-syncopate",
-});
-
-const orbitron = Orbitron({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  variable: "--font-orbitron",
-});
-
-const anta = Anta({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-anta",
-});
-
-const exo2 = Exo_2({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
-  variable: "--font-exo2",
-});
 
 const oxanium = Oxanium({
   subsets: ["latin"],
@@ -52,7 +15,7 @@ const openSans = Open_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "OffiTec SP",
+  title: "OSP",
   description: "OffiTec HVAC Selection Platform",
 };
 
@@ -67,7 +30,7 @@ export default function RootLayout({
       className="h-full antialiased"
       suppressHydrationWarning
     >
-      <body className={`${plusJakartaSans.className} ${robotoCondensed.variable} ${plusJakartaSans.variable} ${syncopate.variable} ${orbitron.variable} ${anta.variable} ${exo2.variable} ${oxanium.variable} ${openSans.variable} min-h-full flex flex-col`} suppressHydrationWarning>{children}</body>
+      <body className={`${oxanium.variable} ${openSans.variable} ${openSans.className} min-h-full flex flex-col`} suppressHydrationWarning>{children}</body>
     </html>
   );
 }
